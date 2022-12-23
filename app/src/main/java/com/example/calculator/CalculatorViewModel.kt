e97a9c4c2ca712365d326c33aed45c8c48882ce6
Fabio Privitera <fabio_privitera@yahoo.it>
2022-12-23 12:45:01 +0200

    adds state

---
+++ app/src/main/java/com/example/calculator/CalculatorViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.calculator
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+//androidx.lifecycle.ViewModel allows to keep the state alive contrary to android activities
+class CalculatorViewModel: ViewModel(){
+
+    var state by mutableStateOf(CalculatorState())
+        private set
+
+}
